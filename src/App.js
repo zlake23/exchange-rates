84c75c202e0938c2bb38afecfc925ec429f4ff82
Zachary Lake <zlake23@gmail.com>
2020-05-25 12:56:43 -0700

    Adds doFetch funciton and select menu functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [selectedRegionTitle, setSelectedRegionTitle] = useState('Europe');
+
+  function doFetch() {
+    console.log('doFetch invoked');
+    fetch('https://api.exchangeratesapi.io/latest')
+        .then(response => response.json())
+        .then(data => {
+            // apiData = Object.entries(data.rates);
+            // apiData.sort();
+            console.log(data);
+            console.log(selectedRegionTitle);
+            // render();
+        });
+}
+doFetch();
+
   return (
     <div className="App">
       <header>
@@ -10,10 +25,10 @@ function App() {
             <h2>Base Currency: 1 EURO</h2>
         </header>
 
-        <div class="Container">
-            <div class="Graph">
-                <div class="Graph-select">
-                    <select onchange="changeRegion()" id="exchange-rate-region">
+        <div className="Container">
+            <div className="Graph">
+                <div className="Graph-select">
+                    <select onChange={event => setSelectedRegionTitle(event.target.value)} id="exchange-rate-region">
                         <option>Europe</option>
                         <option>Asia</option>
                         <option>North America</option>
@@ -21,8 +36,8 @@ function App() {
                         <option>Africa/Oceania</option>
                     </select>
                 </div>
-                <div class="Graph-text" id="country-rate"></div>
-                <div class="Graph-content" id="bar-chart"></div>
+                <div className="Graph-text" id="country-rate"></div>
+                <div className="Graph-content" id="bar-chart"></div>
             </div>
         </div>
     </div>
