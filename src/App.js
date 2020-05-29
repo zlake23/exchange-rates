4021640d85eb2a64b1384d2ac64271873af6a0ae
Zachary Lake <zlake23@gmail.com>
2020-05-28 19:57:32 -0700

    Adds base currency functionality menu

--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import './App.css';
 
 function App() {
   const [selectedRegionTitle, setSelectedRegionTitle] = useState('Europe');
-  const [selectedRegion, setSelectedRegion] = useState({
+  const [selectedBase, setSelectedBase] = useState('EUR');
+  const selectedRegion = {
     'Europe': ['BGN', 'CZK', 'DKK', 'GBP', 'HUF', 'PLN', 'RON', 'SEK', 'CHF', 'ISK', 'NOK', 'HRK', 'RUB'],
     'Asia': ['CNY', 'HKD', 'IDR', 'ILS', 'INR', 'KRW', 'MYR', 'PHP', 'SGD', 'THB', 'TRY'],
     'North America': ['USD', 'CAD', 'MXN'],
     'South America': ['BRL'],
     'Africa/Oceania': ['ZAR', 'AUD', 'NZD'],
-  });
+  };
   let [rates, setRates] = useState([])
 
   function doFetch() {
-    fetch('https://api.exchangeratesapi.io/latest')
+    fetch('https://api.exchangeratesapi.io/latest?base=' + selectedBase)
         .then(response => response.json())
         .then(data => {
           setRates(Object.entries(data.rates));
         });
   }
 
-useEffect(doFetch, [selectedRegionTitle]);
+useEffect(doFetch, [selectedRegionTitle, selectedBase]);
 
   return (
     <div className="App">
@@ -32,17 +33,32 @@ useEffect(doFetch, [selectedRegionTitle]);
         <div className="Container">
             <div className="Graph">
                 <div className="Graph-select">
+                  <div>
+                    <span>Select Continental Region:</span>
                     <select onChange={event => setSelectedRegionTitle(event.target.value)}>
+                        
                         <option>Europe</option>
                         <option>Asia</option>
                         <option>North America</option>
                         <option>South America</option>
                         <option>Africa/Oceania</option>
                     </select>
+                    </div>
+                    <div>
+                    <span>Select base currency:</span>
+                    <select onChange={event => setSelectedBase(event.target.value)}>
+                        <option>EUR</option>
+                        <option>USD</option>
+                        <option>CAD</option>
+                        <option>GBP</option>
+                        <option>AUD</option>
+                    </select>
+                    </div>
                 </div>
                 <div className="Graph-text">
                   {
                     rates
+                    .sort()
                     .filter(rate => selectedRegion[selectedRegionTitle].includes(rate[0]))
                     .map(rate => (
                     <div className="Graph-text" key={rate[0]}>
